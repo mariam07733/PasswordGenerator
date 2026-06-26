f01f2cbf6605bbe00391efa768a706f7a212d704
mariam toure <serayatoure@gmail.com>
2026-06-26 01:52:05 +0000

    Ajout de l'énumération PasswordStrength

--- src/main/java/org/example/PasswordValidator.java
+++ src/main/java/org/example/PasswordValidator.java
@@ -1,4 +1,9 @@
 package org.example;
 
 public class PasswordValidator {
+
+    public static boolean isValid(String password) {
+        return password != null && password.length() >= 8;
+    }
+
 }
